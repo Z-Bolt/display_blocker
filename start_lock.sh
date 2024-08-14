4ae7db100cdf2a469c6b35786337e7e1bb7b239b
Vlad <427departament@gmail.com>
2024-08-14 18:42:20 +0300

    Изменение алгоритма работы блокировщика

--- start_lock.sh
+++ start_lock.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-echo "Start Lock service">>/home/rock/display_blocker/lock.log
-sudo systemctl start screen_lock.service
+
+host=`whoami`
+
+config_path=($(find /home/$host/ $(pwd) -name KlipperScreen.conf ))
+
+STATE=($(cat $config_path | grep "screen_lock" | cut -b 16-))
+if [ $STATE == "True" ]; then
+    echo "Start Lock service">>/home/rock/display_blocker/lock.log
+    sudo systemctl start screen_lock.service
+else
+    sudo systemctl stop screen_lock.service
+fi
