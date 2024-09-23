07691712f3c4feaa80b33a2e5ba6c89a1cfb78f1
Vlad <427departament@gmail.com>
2024-09-23 15:26:35 +0300

    Изменен VendorID по умолчанию на abcd Исправлен алгоритм работы блокировщика

--- start_lock.sh
+++ start_lock.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-config_path=($(find /home/rock/klipper_config -name KlipperScreen.conf ))
-
-STATE=($(cat /home/rock/klipper_config/KlipperScreen.conf | grep "screen_lock" | cut -b 16-))
-if [ $STATE == "True" ]; then
-    echo "Start Lock service">>/home/rock/display_blocker/lock.log
+STATE=($(cat /home/rock/klipper_config/KlipperScreen.conf | grep "True" | cut -b 17-))
+if [[ -n $STATE ]]
+then
+    sudo echo "Start Lock service">>/home/rock/display_blocker/lock.log
     sudo systemctl start screen_lock.service
+else
+    sudo echo "Lock disable">>/home/rock/display_blocker/lock.log
+    sudo systemctl stop screen_lock.service
 fi
