86847df6e7731fcdb75259df2d4617d0b2f3d772
Vlad <427departament@gmail.com>
2025-01-21 18:08:43 +0300

    Обновление работы логов

--- start_lock.sh
+++ start_lock.sh
@@ -3,11 +3,11 @@
 STATE=($(cat /home/pi/printer_data/config/KlipperScreen.conf | grep "True" | cut -b 17-))
 if [[ -n $STATE ]]
 then
-    sudo echo $STATE
+    sudo echo $STATE>>/home/pi/display_blocker/lock.log
     sudo echo "Start Lock service">>/home/pi/display_blocker/lock.log
     sudo systemctl start screen_lock.service
 else
-    sudo echo $STATE
+    sudo echo $STATE>>/home/pi/display_blocker/lock.log
     sudo echo "Lock disable">>/home/pi/display_blocker/lock.log
     sudo systemctl stop screen_lock.service
 fi
